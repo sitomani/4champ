7e3e7a959c6fcf12b9411dcb3dda75b1ac292273
Aleksi Sitomaniemi <aleksi@4champ.net>
2023-10-07 14:30:09 +0300

    Swiftlint: also set up SamplePlayer linting

--- SamplePlayer/SamplePlayer/ViewController.swift
+++ SamplePlayer/SamplePlayer/ViewController.swift
@@ -8,25 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
+
   @IBOutlet weak var modLabel: UILabel!
   @IBOutlet weak var modStack: UIStackView!
   @IBOutlet weak var viewTitle: UILabel!
-  
-  private let modulesUrl: URL
-  private let modulePaths: [String]
-  
+
+  private var modulesUrl: URL?
+  private var modulePaths: [String] = []
+
   let replay = Replay()
-  
+
   override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-    modulesUrl = Bundle.main.url(forResource: "Modules", withExtension: "bundle")!
-    modulePaths = try! FileManager.default.contentsOfDirectory(atPath: modulesUrl.path)
+    if let url = Bundle.main.url(forResource: "Modules", withExtension: "bundle") {
+      modulesUrl = url
+      do {
+        modulePaths = try FileManager.default.contentsOfDirectory(atPath: url.path)
+      } catch {
+        print(error)
+      }
+    }
     super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
   }
-  
+
   required init?(coder: NSCoder) {
-    modulesUrl = Bundle.main.url(forResource: "Modules", withExtension: "bundle")!
-    modulePaths = try! FileManager.default.contentsOfDirectory(atPath: modulesUrl.path)
+    if let url = Bundle.main.url(forResource: "Modules", withExtension: "bundle") {
+      modulesUrl = url
+      do {
+        modulePaths = try FileManager.default.contentsOfDirectory(atPath: url.path)
+      } catch {
+        print(error)
+      }
+    }
     super.init(coder: coder)
   }
 
@@ -36,31 +48,30 @@ class ViewController: UIViewController {
     viewTitle.textColor = .black
 
   }
-  
+
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     replay.initAudio()
-    
+
     modStack.spacing = 8.0
     modStack.distribution = .fillEqually
 
-    let bundle = Bundle.main //init(identifier: "lib.uade.ios")
+    let bundle = Bundle.main // init(identifier: "lib.uade.ios")
     viewTitle.text = bundle.resourcePath ?? ""
 //    NSString *bd = [uadeBundle pathForResource:@"UADERes" ofType:@"bundle"];
 
-    
     // Add module buttons
     modulePaths.forEach { path in
       let btn = UIButton()
       btn.backgroundColor = UIColor.init(red: 0, green: 0x42/255, blue: 0x47/255, alpha: 1)
       let ext = path.split(separator: ".").last
-      btn.setTitle(String(ext ?? "n/a").uppercased() , for: .normal)
+      btn.setTitle(String(ext ?? "n/a").uppercased(), for: .normal)
       btn.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
       modStack.addArrangedSubview(btn)
     }
-    
+
   }
-  
+
   @IBAction func buttonTapped(_ sender: UIButton) {
     if let btn = sender.titleLabel?.text {
       switch btn {
@@ -68,10 +79,10 @@ class ViewController: UIViewController {
         modLabel.text = "no module selected"
         replay.stop()
       default:
-        if let mod = modulePaths.first(where: { path in
+        if let rootUrl = modulesUrl, let mod = modulePaths.first(where: { path in
           path.uppercased().hasSuffix(btn)
         }) {
-          let fullpath = "\(modulesUrl.path)/\(mod)"
+          let fullpath = "\(rootUrl.path)/\(mod)"
           modLabel.text = String(mod.split(separator: "/").last ?? "")
           replay.loadModule(fullpath, type: btn)
           replay.play()
@@ -80,4 +91,3 @@ class ViewController: UIViewController {
     }
   }
 }
-
