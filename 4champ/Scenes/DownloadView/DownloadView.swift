0391381e20bb6371da7629ac200008064e44cf8c
Aleksi Sitomaniemi <aleksi@4champ.net>
2024-02-13 21:45:48 +0200

    SwiftUI frame animation API deprecation fix

--- 4champ/Scenes/DownloadView/DownloadView.swift
+++ 4champ/Scenes/DownloadView/DownloadView.swift
@@ -20,7 +20,7 @@ struct ProgressBar: View {
 
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
                     .foregroundColor(Color(UIColor.systemBlue))
-                    .animation(.linear)
+                    .animation(value > 0.01 ? .linear : nil, value: value)
             }.cornerRadius(2)
         }
     }
