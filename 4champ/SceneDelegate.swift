80f1af9174491265fa177f10ba3f8ece8df40bf1
Marek Hac <marek.hac@marinosoftware.com>
2026-03-02 17:52:38 +0100

    Add entitlements and update the info.plist

--- 4champ/SceneDelegate.swift
+++ 4champ/SceneDelegate.swift
@@ -14,21 +14,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        // Use a UIHostingController as window root view controller
-        if let windowScene = scene as? UIWindowScene {
-            let window = UIWindow(windowScene: windowScene)
-            let managedObjectContext = moduleStorage.managedObjectContext
-            let contentView = PlaylistView(store: PlaylistStore())
-                .environment(\.managedObjectContext, managedObjectContext)
-
-            window.rootViewController = UIHostingController(rootView: contentView)
-
-            self.window = window
-            window.makeKeyAndVisible()
-        }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let rootVC = storyboard.instantiateInitialViewController()
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = rootVC
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
